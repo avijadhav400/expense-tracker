a9cda5bd5a9915c46f63087b922efac5733b3f1c
avijadhav400 <avijadhav400@gmail.com>
2024-07-13 12:20:14 +0530

    Initial setup

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+//Connect to db
+const connectDb = async()=>{
+    const conn = await mongoose.connect("mongodb+srv://avinashjadhav:avinashjadhav@avinash23.sifln8v.mongodb.net/expense-tracker")
+
+    if(conn){
+        console.log("Db connected 🚀");
+    }
+    else{
+        console.log("Db not connected ❌");
+    }
+};
+connectDb()
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res)=>{
+    res.json({
+        message: `Welcome to expense tracker`
+    })
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is runnig on ${PORT}`);
+})
